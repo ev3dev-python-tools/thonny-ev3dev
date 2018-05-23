ceb30768f6c883b7cb015233e3770eb9db19f2c9
Harco Kuppens <h.kuppens@cs.ru.nl>
2018-05-23 02:01:10 +0200

    added logging support to ev3devcontext

--- thonny/shared/ev3devcontext.py
+++ thonny/shared/ev3devcontext.py
@@ -1,5 +1,7 @@
+import sys
+import os
+
 def getEV3API():
-    import os
 
     if os.uname()[1] == 'ev3dev':
         import ev3dev.ev3 as ev3
@@ -15,6 +17,41 @@ def getEV3API():
         else:
             raise Exception("ev3 simulation not yet implemented")
     return ev3
+
+
+
+# 'DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'
+
+def getLogger(timing=False):
+    import logging
+    import datetime as dt
+
+    class MyFormatter(logging.Formatter):
+        converter=dt.datetime.fromtimestamp
+        def formatTime(self, record, datefmt=None):
+            ct = self.converter(record.created)
+            if datefmt:
+                s = ct.strftime(datefmt)
+            else:
+                t = ct.strftime("%Y-%m-%d %H:%M:%S")
+                s = "%s,%03d" % (t, record.msecs)
+            return s
+
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+    handler = logging.StreamHandler() # logging.FileHandler(LOG_FILENAME,mode='w')
+    handler.setLevel(logging.DEBUG)
+
+    if timing:
+        # [loglevel time] msg
+        formatter = MyFormatter(fmt='[%(levelname)s %(asctime)s] %(message)s',datefmt='%H:%M:%S.%f')
+    else:
+        # [loglevel] msg
+        formatter = logging.Formatter('[%(levelname)s] %(message)s')
+
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    return logger
 
 
 
