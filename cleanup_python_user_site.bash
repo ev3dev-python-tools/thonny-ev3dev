d78877ec3db1069b9c8c305e69e344804085aa38
Harco Kuppens <h.kuppens@cs.ru.nl>
2020-02-06 17:08:02 +0100

    cleanup user python folder at root

--- cleanup_python_user_site.bash
+++ cleanup_python_user_site.bash
@@ -1,6 +1,7 @@
 
 
-USERSITE=$(python3 -msite --user-site)
+#USERSITE=$(python3 -msite --user-site)
+USERSITE=$(python3 -msite --user-base)
 if [[ -z "$USERSITE" ]]
 then
     echo "no python user site"
